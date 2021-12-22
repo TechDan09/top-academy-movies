b690097dc5b1ec84ac7089f7d0fd055b4cfa8c38
Daniel Onoriukpe <daniel.onoriukpe@toptal.com>
2021-12-23 01:45:28 +0200

    implementing filter logic in js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log(movies[0]);
+console.log(movies);
+const ui = new UI(movies);
+ui.paint();
+
+const getAllGenres = () => {
+  const result = [...new Set(movies.flatMap(({genres}) => genres))].sort();
+  const length = result.length;
+  const select = document.querySelector('.genre_dropdown');
+  for (let i = 0; i < length; i++) {
+    let option = result[i];
+    let el = document.createElement('option');
+    el.textContent = option;
+    el.value = option;
+    select.appendChild(el);
+  }
+};
+
+const init = () => {
+  getAllGenres();
+};
+
+document.addEventListener('DOMContentLoaded', init);
